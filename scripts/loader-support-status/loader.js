cffb8840f2abdec920112f910ba0dd4b9265554c
Ahmad Amireh <ahmad@amireh.net>
2016-05-21 01:07:23 +0300

    Coerce "sourceMap" to boolean

--- scripts/loader-support-status/loader.js
+++ scripts/loader-support-status/loader.js
@@ -1,13 +1,22 @@
-var fs = require('fs');
+var fs = require('fs-extra');
+var path = require('path');
 
 module.exports = function(s) {
-  var attrs = {};
+  var attrs = extractProperties(this);
+  var filePath = path.resolve(this.query.match(/out=(.*)/)[1]);
 
-  for (var key in this) {
-    attrs[key] = typeof this[key];
-  }
-
-  fs.writeFileSync(this.query.match(/out=(.*)/)[1], JSON.stringify(attrs));
+  fs.ensureDirSync(path.dirname(filePath));
+  fs.writeFileSync(filePath, JSON.stringify(attrs));
 
   return s;
 };
+
+function extractProperties(object, level) {
+  var attrs = {};
+
+  for (var key in object) {
+    attrs[key] = typeof object[key];
+  }
+
+  return attrs;
+}
